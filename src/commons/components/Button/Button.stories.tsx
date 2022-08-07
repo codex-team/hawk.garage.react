2c1e4a3e286544134e2d08fae04c33309edff409
slaveeks <chernyshev2001@icloud.com>
2022-08-08 00:28:52 +0300

    Renamed component file

--- src/commons/components/Button/Button.stories.tsx
+++ src/commons/components/Button/Button.stories.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {ComponentMeta, ComponentStory} from '@storybook/react';
 
-import Button, {ButtonSize, ButtonStyle} from './';
+import Button, {ButtonSize, ButtonStyle} from './Button';
 
 export default {
     title: 'Example/Button',
