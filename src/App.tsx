4cab1d05700902117b15f6231e3d213d0a62003f
Ilya Maroz <37909603+ilyamore88@users.noreply.github.com>
2022-09-06 10:24:10 +0100

    feat(router): add react router v6 lib, create simple example (#33)
    * chore: add react router v6
    
    * feat: connect the app to browser url
    
    * feat: create simple example with the new router
    
    * chore: change version in package.json

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,26 @@
-import { ReactElement, useState } from 'react';
-import reactLogo from './assets/react.svg';
+import React, { ReactElement } from 'react';
 import styles from './App.module.css';
+import {Link, Route, Routes} from "react-router-dom";
+import {Login} from "./pages/Login";
+import {Logout} from "./pages/Logout";
 
 /**
  * Application root component
  */
 function App(): ReactElement {
-  const [count, setCount] = useState(0);
-
   return (
     <div className={styles.app}>
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className={styles.card}>
-        <button onClick={() => setCount((prevState) => prevState + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className={styles.readTheDocs}>
-        Click on the Vite and React logos to learn more
-      </p>
+      <nav>
+        <Link to="/login">Login page</Link>
+        <Link to="/logout">Logout page</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={
+          <div>Click on the link</div>
+        } />
+        <Route path="login" element={<Login />} />
+        <Route path="logout" element={<Logout />} />
+      </Routes>
     </div>
   );
 }
