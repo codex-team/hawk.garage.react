45c40c9ea26e25172441a2003eeb3a3362fae5ee
Vyacheslav Chernyshev <81693471+slaveeks@users.noreply.github.com>
2022-09-21 18:01:50 +0300

    Added checkbox component (#35)
    * Added checkbox component
    
    * Fixed changing checkbox checked, added useEffect for checked props, added checkedState for checkbox
    
    * Using library for svg icons as components, changed styles, using lodash.noop for callback function
    
    * Changed structere of checkbox and styles
    
    * Added onClick to input
    
    * Changed conditional in onChange
    
    * Changed checkbox styles
    
    * Removed useless import
    
    * Fixed animation
    
    * Added className to props for checkbox wrapper
    
    * Using variable for checkbox mark padding

---
+++ src/commons/components/Checkbox/Checkbox.tsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import styles from './Checkbox.module.css';
+import Tick from '../../../assets/tick.svg?component'
+import { noop } from "lodash";
+import cn from "classnames";
+
+interface Props {
+
+  /**
+   * Is checkbox checked
+   */
+  checked?: boolean;
+
+  /**
+   * Is checkbox disabled
+   */
+  disabled?: boolean,
+
+  /**
+   * Callback function for checked checkbox
+   */
+  onChange?: ( state: boolean ) => void;
+
+  /**
+   * Classname for styling wrapper
+   */
+  className?: string;
+}
+
+/**
+ * Checkbox component
+ *
+ * @param props - props of component
+ */
+const Checkbox: React.FC<Props> = ({ disabled, checked, onChange = noop, className, ...props }) => {
+
+  const onClick = () => {
+    onChange(!checked);
+  }
+
+  return (
+    <div className={cn(styles.wrapper, className)}>
+      <input type={'checkbox'}
+             className={styles.checkbox}
+             checked={checked}
+             onClick={onClick}
+             disabled={disabled}
+             {...props}/>
+      <Tick className={styles.mark}/>
+    </div>
+  );
+};
+
+export default Checkbox;
