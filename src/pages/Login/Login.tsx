62283947a495ecea08c8e55d401ba30e9bb776d7
slaveeks <chernyshev2001@icloud.com>
2022-09-22 18:39:39 +0300

    Added login page to pages

---
+++ src/pages/Login/Login.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import styles from "./Login.module.css"
+import {Base} from "../../commons/layouts/auth/base/Base";
+import Button, { ButtonStyle } from "../../commons/components/Button/Button";
+import {Fieldset} from "../../commons/components/Fieldset/Fieldset";
+
+export const Login: React.FC = () => {
+  return (
+    <div className={styles.container}>
+      <Base>
+        <Fieldset className={styles.fieldset} label={'email address'} type={'email'} name={'email'} placeholder={'name@best-team.com'}/>
+        <Fieldset className={styles.fieldset} label={'password'} type={'password'} name={'password'} placeholder={'********'}/>
+        <div className={styles.action}>
+          <Button styleType={ButtonStyle.Submit}>
+            Log in
+          </Button>
+          <a href={"/recover"}>Recover password</a>
+        </div>
+      </Base>
+    </div>
+  );
+}
