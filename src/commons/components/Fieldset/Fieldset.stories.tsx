349c66f1d9787ec8d05dcf7c85c54f36da038daa
slaveeks <chernyshev2001@icloud.com>
2022-09-23 14:08:35 +0300

    Added stoybooks for Delimiter, Fieldset and Login page

---
+++ src/commons/components/Fieldset/Fieldset.stories.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import {ComponentMeta, ComponentStory} from '@storybook/react';
+
+import { Fieldset } from "./Fieldset";
+
+export default {
+  title: 'Input/Fieldset',
+  component: Fieldset,
+} as ComponentMeta<typeof Fieldset>;
+
+const Template: ComponentStory<typeof Fieldset> = (props) => <Fieldset
+  {...props} />;
+
+export const Default = Template.bind({});
+
+Default.args = {
+  label: 'Email Address',
+  placeholder: 'name@best-team.com',
+  type: 'email',
+  name: 'email',
+}
