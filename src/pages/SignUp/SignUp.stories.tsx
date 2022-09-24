6cd0a48d234dcb7781f039a5c38fa3d953986459
ilyamore88 <moroz.ilya.olegovich@gmail.com>
2022-09-24 15:25:54 +0100

    feat: add SignUp page component

---
+++ src/pages/SignUp/SignUp.stories.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import {ComponentMeta, ComponentStory} from '@storybook/react';
+import {SignUp} from "./SignUp";
+import { withRouter } from 'storybook-addon-react-router-v6';
+
+export default {
+  title: 'Pages/SignUp',
+  component: SignUp,
+  decorators: [withRouter],
+  parameters: {
+    reactRouter: {
+      routePath: '/sign-up'
+    }
+  }
+} as ComponentMeta<typeof SignUp>;
+
+const Template: ComponentStory<typeof SignUp> = () => <SignUp />;
+
+export const Default = Template.bind({});
