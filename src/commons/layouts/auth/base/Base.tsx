f0043bd1b358ddcf7843343ac69144cd7ee64ff7
slaveeks <chernyshev2001@icloud.com>
2022-09-27 17:41:52 +0300

    Some styles changes, using variables

--- src/commons/layouts/auth/base/Base.tsx
+++ src/commons/layouts/auth/base/Base.tsx
@@ -8,7 +8,7 @@ interface Props extends React.PropsWithChildren {}
 export const Base: React.FC<Props> = ({children}) => {
   return (
     <div className={styles.container}>
-      <Text weight={"bold"} size={"m"} className={styles.title}>Hawk</Text>
+      <Text weight="bold" size="m" className={styles.title}>Hawk</Text>
       <Text className={styles.caption}>Time for quality</Text>
       <div className={styles.login}>
         <div className={styles.picture}/>
