5752b9f106bd476f4203943c42cdc2dc6654408a
ilyamore88 <moroz.ilya.olegovich@gmail.com>
2022-10-02 17:15:17 +0100

    feat: add AuthProvider to the app

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,21 @@ import { Route, Routes } from 'react-router-dom';
 import { Login } from '@/pages/Login/Login';
 import cn from 'classnames';
 import { SignUp } from '@/pages/SignUp/SignUp';
+import { AuthProvider, authProviderValue } from '@/providers/AuthProvider';
 
 /**
  * Application root component
  */
 function App(): ReactElement {
   return (
-    <div className={cn('hds-root', 'hds-root-colors-dark', styles.app)}>
-      <Routes>
-        <Route path="login" element={<Login />} />
-        <Route path="signup" element={<SignUp />} />
-      </Routes>
-    </div>
+    <AuthProvider value={authProviderValue}>
+      <div className={cn('hds-root', 'hds-root-colors-dark', styles.app)}>
+        <Routes>
+          <Route path="login" element={<Login />} />
+          <Route path="signup" element={<SignUp />} />
+        </Routes>
+      </div>
+    </AuthProvider>
   );
 }
 
